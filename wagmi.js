7ede2c0bd8840033e4758565e32da1cfcfc91e59
ankurrrrr7 <ankurjha956@gmail.com>
2025-05-24 00:37:34 +0530

    Just for check

--- wagmi.js
+++ wagmi.js
@@ -34,6 +34,10 @@ app.post('/wagmi', async (request, reply) => {
     status: "success"
   });
 });
+app.get('/', async (request, reply) => {
+  return { status: 'ok', message: 'Hello World' };
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
